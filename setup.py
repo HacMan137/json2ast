33d99f919b9a7d0605dd01f18b94828a61d72cbe
Adam Nichols <adam.nichols@peratonlabs.com>
2022-06-17 08:06:54 -0400

    Updated version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="json2ast",
-    version="1.0.0",
+    version="1.0.1",
     author="Adam Nichols",
     description="The opposite of ast2json",
     long_description=long_description,
